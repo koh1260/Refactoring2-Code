e7aaaecf928fe7609882b1cc4f1b1725e3af95c8
Haesung <koh1260@naver.com>
2023-07-16 01:43:33 +0900

    totalAmount 의미 있는 이름으로 변경

--- chapter 1/statement.ts
+++ chapter 1/statement.ts
@@ -11,11 +11,11 @@ export const statement = (invoice: Invoice, plays: Play) => {
       perf.audience
     }석)\n`;
   }
-  result += `총액 : ${usd(appleSauce())}\n`;
+  result += `총액 : ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
-  function appleSauce() {
+  function totalAmount() {
     let totalAmount = 0;
     for(let perf of invoice.performances) {
       totalAmount += amountFor(perf);
