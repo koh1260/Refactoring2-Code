ac7fa9280c599ce31e551ff0ef5b2f201067c571
Haesung <koh1260@naver.com>
2023-07-15 21:09:49 +0900

    함수 추출하기: switch case로직 -> amountFor

--- chapter 1/statement.ts
+++ chapter 1/statement.ts
@@ -1,5 +1,5 @@
-import { Invoice, invoices } from "./invoices";
-import { Play, plays } from "./plays";
+import { Invoice, Performances, invoices } from "./invoices";
+import { Info, Play, plays } from "./plays";
 
 ("use strict");
 export const statement = (invoice: Invoice, plays: Play) => {
@@ -14,25 +14,10 @@ export const statement = (invoice: Invoice, plays: Play) => {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 1000 + 500 * (perf.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
+    // 포인트를 적립한다.
     volumeCredit += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === play.type) volumeCredit += Math.floor(perf.audience / 5);
 
     result += `${play.name}: ${format(thisAmount / 100)} (${
@@ -43,6 +28,28 @@ export const statement = (invoice: Invoice, plays: Play) => {
   result += `총액 : ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredit}점\n`;
   return result;
+}
+
+const amountFor = (perf: Performances, play: Info) => {
+  let thisAmount = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 1000 + 500 * (perf.audience - 20);
+      }
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${play.type}`);
+  }
+  return thisAmount;
 }
 
 console.log(statement(invoices, plays));
