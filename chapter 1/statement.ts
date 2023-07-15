da77003996cfca55463ff4f364e97b7042fe6182
Haesung <koh1260@naver.com>
2023-07-16 01:32:52 +0900

    volumeCredits 계산 함수로 추출

--- chapter 1/statement.ts
+++ chapter 1/statement.ts
@@ -13,14 +13,18 @@ export const statement = (invoice: Invoice, plays: Play) => {
     }석)\n`;
     totalAmount += amountFor(perf);
   }
-  let volumeCredits = 0;
-  for(let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
-
+  let volumeCredits = totalVolumeCredits();
   result += `총액 : ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for(let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
 
   function usd(aNumber: number) {
     return new Intl.NumberFormat("en-US", {
