b862a89fda8787fbf36f438666d7cf2d4f6efb9c
Haesung <koh1260@naver.com>
2023-07-15 20:59:15 +0900

    statement 테스트 코드 추가

---
+++ chapter 1/statement.test.ts
@@ -0,0 +1,52 @@
+import { Invoice } from "./invoices";
+import { Play } from "./plays";
+import { statement } from "./statement";
+
+describe("statements 테스트", () => {
+  it("statements", () => {
+    // given
+    const invoices: Invoice = {
+      customer: "CUSTOMER",
+      performances: [
+        { playID: "play1", audience: 40 },
+        { playID: "play2", audience: 30 },
+      ],
+    };
+    const plays: Play = {
+      play1: { name: "PLAY1", type: "tragedy" },
+      play2: { name: "PLAY2", type: "comedy" },
+    };
+
+    // when
+    const result = statement(invoices, plays);
+    console.log(result);
+
+    const expectResult = `청구 내역 (고객명: CUSTOMER)
+PLAY1: $500.00 (40석)
+PLAY2: $360.00 (30석)
+총액 : $860.00
+적립 포인트: 16점\n`;
+
+    // then
+    expect(result).toEqual(expectResult);
+  });
+
+	it('statement 존재하지 않는 장르', () => {
+		// given
+		const invoices: Invoice = {
+			customer: 'CUSTOMER',
+			performances: [
+				{ playID: 'play1', audience: 30 },
+			],
+		};
+		const play: Play = {
+			play1: { name: 'PLAY1', type: 'UNKNOWN', },
+		};
+
+		// when
+		// then
+		expect(() => {
+			statement(invoices, play);
+		}).toThrowError(new Error('알 수 없는 장르: UNKNOWN'));
+	})
+});
