55f6b7d00f5785e6bb0f1008b4fc5c6e824f7307
Haesung <koh1260@naver.com>
2023-07-16 14:55:33 +0900

    amountFor 함수 클래스로 이동

--- chapter 1/createStatementDate.ts
+++ chapter 1/createStatementDate.ts
@@ -11,40 +11,19 @@ export default function createStatementData(invoice: Invoice, plays: Play) {
   return result;
 
   function enrichPerformace(aPerformance: Performances) {
-    const calculator = new PerformanceCalculator(aPerformance);
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance),
+    );
     const result: any = Object.assign({}, aPerformance);
     result.play = playFor(result);
-    result.amount = amountFor(result);
+    result.amount = calculator.getAmount();
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
   function playFor(aPerformance: Performances) {
     return plays[aPerformance.playID];
-  }
-
-  // aPerformance 명확한 이름
-  function amountFor(aPerformance: any) {
-    // 반환 값은 result로
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 1000 + 500 * (aPerformance.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
   }
 
   function volumeCreditsFor(perf: any) {
--- chapter 1/performance-calculator.ts
+++ chapter 1/performance-calculator.ts
@@ -3,8 +3,33 @@ import { Info } from "./plays";
 
 export class PerformanceCalculator{
     performance: Performances;
+    play: Info;
 
-    constructor(aPerformance: Performances) {
+    constructor(aPerformance: Performances, aPlay: Info) {
         this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    getAmount() {
+        // 반환 값은 result로
+    let result = 0;
+
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 1000 + 500 * (this.performance.audience - 20);
+        }
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+    return result;
     }
 }
