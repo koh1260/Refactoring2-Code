69b25cb0c918018d6327f6294adcd10fbc9bfa2a
Haesung <koh1260@naver.com>
2023-07-19 00:43:32 +0900

    province, 경계 값 테스트 추가

--- ch-4/province.test.ts
+++ ch-4/province.test.ts
@@ -8,7 +8,7 @@ describe('province', () => {
         // 테스트 전에 항상 실행.
         // 테스트마다 새로운 객체 할당 -> 테스트 간의 의존성 X
         asia = new Province(sampleProvinceData())
-    })
+    });
 
     it('shortfall', () => {
         // when
@@ -16,7 +16,7 @@ describe('province', () => {
 
         // then
         expect(shortfall).toEqual(5);
-    })
+    });
 
     it('profit', () => {
         // when 
@@ -24,7 +24,7 @@ describe('province', () => {
 
         // then
         expect(profit).toEqual(230);
-    })
+    });
     
     it('change production', () => {
         // when
@@ -33,5 +33,24 @@ describe('province', () => {
         // then
         expect(asia.shortfall).toEqual(-6);
         expect(asia.profit).toEqual(330);
-    })
+    });
+
+    // 0, 음수 등 경계 값들 테스트 -> 특이 상황 발견 가능성 ^
+    it('zero demand', () => {
+        // when
+        asia.demand = 0;
+
+        // given
+        expect(asia.shortfall).toEqual(-25);
+        expect(asia.profit).toEqual(0);
+    });
+
+    it('negative demand', () => {
+        // when
+        asia.demand = -1;
+
+        // then
+        expect(asia.shortfall).toEqual(-26);
+        expect(asia.profit).toEqual(-10);
+    });
 })
