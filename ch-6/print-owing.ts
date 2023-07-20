7f6a1987f719e3899b4a9c42b2a6b1311aae7622
Haesung <koh1260@naver.com>
2023-07-20 21:32:48 +0900

    outstanding 계산 로직 함수로 추출 (문장 슬라이드)

--- ch-6/print-owing.ts
+++ ch-6/print-owing.ts
@@ -3,23 +3,10 @@ import { invoices } from "./invoices";
 import { Clock } from "./clock";
 
 function printOwing(invoice: Invoices) {
-  let outstanding = 0;
-
   printBanner();
-
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
-
-  recordDueDate(invoice)
-
-//   const today = Clock.today;
-
-//   invoice.duDate = new Date(
-//     today.getFullYear(),
-//     today.getMonth(),
-//     today.getDate() + 30
-//   );
+  let outstanding = calcOutstanding(invoice);
+  
+  recordDueDate(invoice);
 
   printDetail(invoice, outstanding);
 
@@ -30,19 +17,27 @@ function printOwing(invoice: Invoices) {
   }
 }
 
+function calcOutstanding(invoice: Invoices) {
+  let outstanding = 0;
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+  return outstanding;
+}
+
 function recordDueDate(invoice: Invoices) {
-    const today = Clock.today;
-    invoice.duDate = new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate() + 30
-      );
+  const today = Clock.today;
+  invoice.duDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
 }
 
 function printDetail(invoice: Invoices, outstanding: number) {
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`);
-    console.log(`마감일: ${invoice.duDate!.toLocaleDateString()}`);
-  }
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.duDate!.toLocaleDateString()}`);
+}
 
 printOwing(invoices);
