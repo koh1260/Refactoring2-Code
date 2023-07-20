c5e3deda5c3c0244ab29515899806eb598d096a0
Haesung <koh1260@naver.com>
2023-07-20 22:14:08 +0900

    shipping 추출

--- ch-6/변수 추출하기/price.ts
+++ ch-6/변수 추출하기/price.ts
@@ -5,10 +5,9 @@ export interface Order {
 
 export function price(order: Order) {
   const basePrice = order.quantity * order.itemPrice;
-  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-  return (
-    basePrice -
-    quantityDiscount +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  
+  return basePrice - quantityDiscount + shipping;
 }
