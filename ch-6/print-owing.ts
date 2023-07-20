56d5c78db94521489423b47679eeed721aa8e6f2
Haesung <koh1260@naver.com>
2023-07-20 21:24:07 +0900

    세부 사항 출력 함수로 추출

--- ch-6/print-owing.ts
+++ ch-6/print-owing.ts
@@ -19,9 +19,13 @@ function printOwing(invoice: Invoices) {
     today.getDate() + 30
   );
 
-  console.log(`고객명: ${invoice.customer}`);
-  console.log(`채무액: ${outstanding}`);
-  console.log(`마감일: ${invoice.duDate.toLocaleDateString()}`);
+  printDetail();
+
+  function printDetail() {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.duDate!.toLocaleDateString()}`);
+  }
 
   function printBanner() {
     console.log("*******************");
@@ -29,7 +33,5 @@ function printOwing(invoice: Invoices) {
     console.log("*******************");
   }
 }
-
-
 
 printOwing(invoices);
