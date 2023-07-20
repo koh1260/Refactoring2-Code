cd76cfe478b3f7a1d30668a4740623eb45a458f4
Haesung <koh1260@naver.com>
2023-07-20 22:13:23 +0900

    quantityDiscount 추출

--- ch-6/변수 추출하기/price.ts
+++ ch-6/변수 추출하기/price.ts
@@ -5,9 +5,10 @@ export interface Order {
 
 export function price(order: Order) {
   const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
   return (
     basePrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    quantityDiscount +
     Math.min(order.quantity * order.itemPrice * 0.1, 100)
   );
 }
