10990ce4f16932ea2f8a4f60475d64354e9a2dcf
Haesung <koh1260@naver.com>
2023-07-20 22:51:42 +0900

    주석 삭제

--- ch-6/변수 캡슐화/cap.ts
+++ ch-6/변수 캡슐화/cap.ts
@@ -5,7 +5,6 @@ interface Owner {
 
 let defaultOwnerData: Owner = {firstName: '마틴', lastName: '파울러'};
 
-// 참조를 반환하므로, defaultOwner().firstName 등으로 변경 가능
 export function defaultOwner() {
     return Object.assign({}, defaultOwnerData);
 }
