f02f79912c35faa15a1f8d05bd7894ff29d144c9
Haesung <koh1260@naver.com>
2023-07-20 22:51:31 +0900

    데이터 복제본 반환 -> 불변성

--- ch-6/변수 캡슐화/cap.ts
+++ ch-6/변수 캡슐화/cap.ts
@@ -7,7 +7,7 @@ let defaultOwnerData: Owner = {firstName: '마틴', lastName: '파울러'};
 
 // 참조를 반환하므로, defaultOwner().firstName 등으로 변경 가능
 export function defaultOwner() {
-    return defaultOwnerData;
+    return Object.assign({}, defaultOwnerData);
 }
 export function setDefaultOwner(arg: Owner) {
     defaultOwnerData = arg;
