305e8c122a25d58e50dfe358264775ca548b0fe6
Haesung <koh1260@naver.com>
2023-07-20 21:22:58 +0900

    중첩 함수로 변경

--- ch-6/print-owing.ts
+++ ch-6/print-owing.ts
@@ -22,12 +22,14 @@ function printOwing(invoice: Invoices) {
   console.log(`고객명: ${invoice.customer}`);
   console.log(`채무액: ${outstanding}`);
   console.log(`마감일: ${invoice.duDate.toLocaleDateString()}`);
+
+  function printBanner() {
+    console.log("*******************");
+    console.log("***** 고객 채무 *****");
+    console.log("*******************");
+  }
 }
 
-function printBanner() {
-  console.log("*******************");
-  console.log("***** 고객 채무 *****");
-  console.log("*******************");
-}
+
 
 printOwing(invoices);
