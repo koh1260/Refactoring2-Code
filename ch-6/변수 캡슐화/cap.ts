df73d5c56ad7c58278c77f6d2e37c654cec6e4ad
Haesung <koh1260@naver.com>
2023-07-20 22:38:51 +0900

    get, set 메서드로 캡슐화

--- ch-6/변수 캡슐화/cap.ts
+++ ch-6/변수 캡슐화/cap.ts
@@ -1,1 +1,12 @@
-export let defaultOwner = {firstName: '마틴', lastName: '파울러'};
+interface Owner {
+    firstName: string;
+    lastName: string;
+}
+
+let defaultOwnerData: Owner = {firstName: '마틴', lastName: '파울러'};
+export function defaultOwner() {
+    return defaultOwnerData;
+}
+export function setDefaultOwner(arg: Owner) {
+    defaultOwnerData = arg;
+}
