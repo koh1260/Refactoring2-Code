8f43ed08460c25664dbb71fa3a826db5d9357296
Haesung <koh1260@naver.com>
2023-07-22 23:16:33 +0900

    client1 Reading.baseCharge 적용, 임시 변수 인라인

--- ch-6/여러 함수를 클래스로 묶기/clients.ts
+++ ch-6/여러 함수를 클래스로 묶기/clients.ts
@@ -1,9 +1,9 @@
 import { Reading, acquirRading, baseRate, taxThreshord } from "./functions";
 
 export const client1 = () => {
-  const aReading = acquirRading();
-  const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
-  const taxableCharge = Math.max(0, base - taxThreshord(aReading.year));
+  const rawReading = acquirRading();
+  const aReading = new Reading(rawReading.customer, rawReading.quantity, rawReading.month, rawReading.year)
+  const taxableCharge = Math.max(0, aReading.baseCharge - taxThreshord(aReading.year));
 
   return taxableCharge;
 };
@@ -13,7 +13,7 @@ export const client2 = () => {
   const aReading = new Reading(rawReading.customer, rawReading.quantity, rawReading.month, rawReading.year)
   const baseCharge = aReading.baseCharge;
 
-    return baseCharge;
+  return baseCharge;
 };
 
 export const client3 = () => {
