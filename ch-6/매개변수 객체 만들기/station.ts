2ed32402adda8cd6b1e1eaf392f63dc2c727e425
Haesung <koh1260@naver.com>
2023-07-22 17:49:23 +0900

    데이터, 타입 분리

---
+++ ch-6/매개변수 객체 만들기/station.ts
@@ -0,0 +1,20 @@
+export interface Station {
+  name: string;
+  readings: Reading[];
+}
+
+interface Reading {
+  temp: number;
+  time: Date;
+}
+
+export const station: Station = {
+  name: "ZB1",
+  readings: [
+    { temp: 47, time: new Date("2016-11-10 09:10") },
+    { temp: 53, time: new Date("2016-11-10 09:20") },
+    { temp: 58, time: new Date("2016-11-10 09:30") },
+    { temp: 53, time: new Date("2016-11-10 09:40") },
+    { temp: 51, time: new Date("2016-11-10 09:50") },
+  ],
+};
