06c6d7dc3986e29492787788d06544b90d284e66
Haesung <koh1260@naver.com>
2023-07-22 18:11:27 +0900

    NumberRange 클래스 추가

--- ch-6/매개변수 객체 만들기/reading-outside-range.ts
+++ ch-6/매개변수 객체 만들기/reading-outside-range.ts
@@ -1,6 +1,26 @@
 import { Station } from "./station";
 
-function readingOutsideRange(station: Station, min: number, max: number) {
-  return station.readings
-    .filter(r => r.temp < min || r.temp > max);
+class NumberRange {
+  private _min: number;
+  private _max: number;
+
+  constructor(min: number, max: number, range: NumberRange) {
+    this._min = min;
+    this._max = max;
+  }
+  
+  get min() {
+    return this._min;
+  }
+  get max() {
+    return this._max;
+  }
+}
+
+export function readingOutsideRange(
+  station: Station,
+  min: number,
+  max: number
+) {
+  return station.readings.filter((r) => r.temp < min || r.temp > max);
 }
