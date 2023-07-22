20af9a55d5fe486128851857ca2f7d68cad586e4
Haesung <koh1260@naver.com>
2023-07-23 00:59:59 +0900

    client1, base 인라인

--- ch-6/여러 함수를 변환 함수로 묶기/clients.ts
+++ ch-6/여러 함수를 변환 함수로 묶기/clients.ts
@@ -3,8 +3,7 @@ import { Reading, acquirRading, baseRate, calculatorBaseCharge, enrichReading, t
 export const client1 = () => {
   const rawReading = acquirRading();
   const aReading = enrichReading(rawReading);
-  const base = aReading.baseCharge;
-  const taxableCharge = Math.max(0, base! - taxThreshord(aReading.year));
+  const taxableCharge = Math.max(0, aReading.baseCharge! - taxThreshord(aReading.year));
 
   return taxableCharge;
 };
