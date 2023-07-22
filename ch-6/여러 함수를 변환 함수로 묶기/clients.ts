c9c49f0e399238c0b9d285bccde5038aa3cedb77
Haesung <koh1260@naver.com>
2023-07-23 00:59:17 +0900

    enrich, client1에 적용

--- ch-6/여러 함수를 변환 함수로 묶기/clients.ts
+++ ch-6/여러 함수를 변환 함수로 묶기/clients.ts
@@ -1,9 +1,10 @@
 import { Reading, acquirRading, baseRate, calculatorBaseCharge, enrichReading, taxThreshord } from "./functions";
 
 export const client1 = () => {
-  const aReading = acquirRading();
-  const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
-  const taxableCharge = Math.max(0, base - taxThreshord(aReading.year));
+  const rawReading = acquirRading();
+  const aReading = enrichReading(rawReading);
+  const base = aReading.baseCharge;
+  const taxableCharge = Math.max(0, base! - taxThreshord(aReading.year));
 
   return taxableCharge;
 };
